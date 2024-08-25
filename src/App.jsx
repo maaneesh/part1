c724888a79cf7f9cf41d3a6ff969f6b10ec7f621
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-25 19:35:58 -0400

    start of chapter part1d

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import Content from "./content";
-import Header from "./header";
+import { useState } from "react";
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
+
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+
+    setCounter(0);
+  };
 
   return (
-    <div>
-      <Header courseName={course} />
-      <Content parts={parts} />
-    </div>
+    <>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text={"Plus"} />
+      <Button onClick={setToZero} text={"Zero"} />
+      <Button onClick={decreaseByOne} text={"Minus"} />
+    </>
   );
 };
 
